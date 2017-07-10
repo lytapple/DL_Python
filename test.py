4204005745ea26c89cdaf8bc7bac88d682fadb46
lyt <yt@lytdeMac-mini.local>
2017-07-10 17:56:13 +0800

    更改第一章

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import numpy as np
 dataset_filename = "affinity_dataset.txt"
 X = np.loadtxt(dataset_filename)
-print(X[:5])
+num_apple_purchases = 0
+for sample in X:
+    if sample[3] == 1:
+        num_apple_purchases += 1
+print("{0} people bought apples".format(num_apple_purchases))
