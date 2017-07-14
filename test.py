e970cf0cd8578c538a0d624252b9b9094b793f4a
lyt <yt@lytdeMac-mini.local>
2017-07-14 17:55:20 +0800

    更新第四章

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-def enroll(a,b,c=1,d=2):
-    print(a,b,c,d)
-enroll(10,1)
-enroll(1,5)
-enroll(1,2,3)
-enroll(1,4,d=6)
-
-def calc(numbers):
-    sum = 0
-    for n in numbers:
-        sum = sum + n * n
-    return sum
-d = calc([3])
-print([1,2,3][1:2])
-from matplotlib import pyplot as plt
-plt.plot([1,2,3] , [1,2,3], '-o')
+def comp(x, y):
+    if x < y:
+        return 1
+    elif x > y:
+        return -1
+    else:
+        return 0
+nums = [3, 2, 8 ,0 , 1]
+nums.sort(comp)
+print(nums)
